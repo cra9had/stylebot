88de0da25e367a6fb9b920fd1843d5d2f37a562b
Khaiam <khaiam@mail.ru>
2024-03-20 17:07:29 +0300

    search queries tested

--- test/test_gpt.py
+++ test/test_gpt.py
@@ -7,6 +7,12 @@ class ChatGPTTest(unittest.IsolatedAsyncioTestCase):
     def setUp(self):
         self.gpt = ChatGPT()
 
+    async def test_search_queries(self):
+        queries = await self.gpt.get_search_queries(
+            "Хочу образ из джинс и футболки и обуви", "мужчина"
+        )
+        self.assertEqual(len(queries), 3)
+
     async def test_chat(self):
         answer = await self.gpt._chat("Ping!")
         self.assertEqual(answer, "Pong!")
