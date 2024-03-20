71d471e84a98e1492c4bf9cb498e1935eed7d19e
Khaiam <khaiam@mail.ru>
2024-03-20 15:12:29 +0300

    reorder_imports

--- wb/api.py
+++ wb/api.py
@@ -1,22 +1,30 @@
+import asyncio
 import json
-from typing import Literal, Optional, Any, Generator
-from loguru import logger
-from .utils import get_query_id_for_search, image_url
 from dataclasses import dataclass
 from itertools import product as _product
+from typing import Any
+from typing import Generator
+from typing import Literal
+from typing import Optional
+
 import aiohttp
-import asyncio
+from loguru import logger
+
+from .utils import get_query_id_for_search
+from .utils import image_url
 
 
-DEFAULT_USER_AGENT = ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-                      "(KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36")
+DEFAULT_USER_AGENT = (
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
+    "(KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
+)
 DEFAULT_RETRIES_NUM = 3
-DEFAULT_ERROR_SLEEP_TIME = 5    # In seconds
+DEFAULT_ERROR_SLEEP_TIME = 5  # In seconds
 
 
 @dataclass
 class Product:
-    id: int    # Артикул товара WildBerries
+    id: int  # Артикул товара WildBerries
     price: int
     name: str
     image_url: str
@@ -40,15 +48,20 @@ class WildBerriesAPI:
         products = []
         for product in data:
             products.append(
-                Product(id=product.get("id"),
-                        price=product.get("sizes")[0].get("price").get("basic") // 100,     # TODO: unsafe, maybe refactor?!
-                        name=product.get("name"),
-                        image_url=image_url(product.get("id"), "BIG"))
+                Product(
+                    id=product.get("id"),
+                    price=product.get("sizes")[0].get("price").get("basic")
+                    // 100,  # TODO: unsafe, maybe refactor?!
+                    name=product.get("name"),
+                    image_url=image_url(product.get("id"), "BIG"),
+                )
             )
         return products
 
     @staticmethod
-    def get_combinations(*products: list[Product], max_repeats: int = 3) -> Generator[list[Product]]:
+    def get_combinations(
+        *products: list[Product], max_repeats: int = 3
+    ) -> Generator[list[Product]]:
         # TODO: test
         """
         Возращает комбинации одежды. В *products перечисляем list[Product]
@@ -59,14 +72,12 @@ class WildBerriesAPI:
                 yield combination
 
     async def search(self, query: str, page: int = 1) -> list[Product]:
-        headers = {
-            "x-queryid": get_query_id_for_search()
-        }
+        headers = {"x-queryid": get_query_id_for_search()}
         params = {
             "ab_testid": "new_pricing",
-            "appType": 1,   # 1 - DESKTOP, 32 - ANDROID, 64 - IOS
+            "appType": 1,  # 1 - DESKTOP, 32 - ANDROID, 64 - IOS
             "curr": "rub",
-            "dest": -1257786,    # MOSCOW
+            "dest": -1257786,  # MOSCOW
             "query": query,
             "resultset": "catalog",
             "sort": "popular",
@@ -77,7 +88,9 @@ class WildBerriesAPI:
         if page != 1:
             params["page"] = page
         url = "https://search.wb.ru/exactmatch/ru/common/v5/search"
-        response = await self._request(url=url, request_method="get", params=params, headers=headers)
+        response = await self._request(
+            url=url, request_method="get", params=params, headers=headers
+        )
         return self.search_data_to_products(json.loads(response))
 
     async def _get_new_session(self) -> aiohttp.ClientSession:
@@ -92,9 +105,15 @@ class WildBerriesAPI:
             self._session = await self._get_new_session()
         return self._session
 
-    async def _request(self, url: str, request_method: Literal['get', 'post'] = "get",
-                       params: Optional[dict] = None, data: Optional[dict] = None,
-                       retries: int = DEFAULT_RETRIES_NUM, **kwargs) -> Any:
+    async def _request(
+        self,
+        url: str,
+        request_method: Literal["get", "post"] = "get",
+        params: Optional[dict] = None,
+        data: Optional[dict] = None,
+        retries: int = DEFAULT_RETRIES_NUM,
+        **kwargs,
+    ) -> Any:
         await self._get_session()
         if request_method == "post":
             method = self._session.post
@@ -111,9 +130,10 @@ class WildBerriesAPI:
             logger.error(f"Error occupied while sending request to {url}:\n{e}")
             if retries > 0:
                 await asyncio.sleep(DEFAULT_ERROR_SLEEP_TIME)
-                return await self._request(url, request_method, params, data, retries - 1, **kwargs)
+                return await self._request(
+                    url, request_method, params, data, retries - 1, **kwargs
+                )
             raise
 
     async def close(self):
         await self._session.close()
-
