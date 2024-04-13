1979cd775e715a255e2326ddfa9bc6d855299d6b
Khaiam <khaiam@mail.ru>
2024-04-13 22:00:48 +0300

    fixed subscription

--- bot/keyboards/payment_kbs.py
+++ bot/keyboards/payment_kbs.py
@@ -9,19 +9,15 @@ def get_subscription_keyboard() -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.button(
         text="Приобрести подписку",
-        callback_data="get_subscription",  # TODO: change callback_data
+        callback_data="go_buy_menu",
     )
     return builder.as_markup()
 
 
 def get_payment_main_menu_kb():
     builder = InlineKeyboardBuilder()
-    builder.button(
-        text="🛒Купить подписку", callback_data='go_buy_menu'
-    )
-    builder.button(
-        text="Вернуться назад", callback_data='go_back_profile_menu'
-    )
+    builder.button(text="🛒Купить подписку", callback_data="go_buy_menu")
+    builder.button(text="Вернуться назад", callback_data="go_back_profile_menu")
     builder.adjust(2)
 
     return builder.as_markup()
@@ -30,21 +26,23 @@ def get_payment_main_menu_kb():
 def get_tariffs_kb():
     builder = InlineKeyboardBuilder()
     builder.button(
-        text='🙂🕯 300 образов в день', callback_data=SubTariffFactory(**Subscriptions.light.value)
+        text="🙂🕯 300 образов в день",
+        callback_data=SubTariffFactory(**Subscriptions.light.value),
     )
     builder.button(
-        text='😊💡500 образов в день', callback_data=SubTariffFactory(**Subscriptions.medium.value)
+        text="😊💡500 образов в день",
+        callback_data=SubTariffFactory(**Subscriptions.medium.value),
     )
     builder.button(
-        text='🤩🔥1000 образов в день', callback_data=SubTariffFactory(**Subscriptions.rare.value)
+        text="🤩🔥1000 образов в день",
+        callback_data=SubTariffFactory(**Subscriptions.rare.value),
     )
     builder.button(
-        text='🌈💥Безлимит', callback_data=SubTariffFactory(**Subscriptions.unlimited.value)
+        text="🌈💥Безлимит",
+        callback_data=SubTariffFactory(**Subscriptions.unlimited.value),
     )
 
-    builder.button(
-        text='Вернуться назад', callback_data='go_payment_menu'
-    )
+    builder.button(text="Вернуться назад", callback_data="go_payment_menu")
 
     builder.adjust(1)
 
@@ -54,13 +52,9 @@ def get_tariffs_kb():
 def get_one_tarif_kb():
     builder = InlineKeyboardBuilder()
 
-    builder.button(
-        text='Купить подписку', callback_data='buy_sub_menu'
-    )
+    builder.button(text="Купить подписку", callback_data="buy_sub_menu")
 
-    builder.button(
-        text='Вернуться назад', callback_data='go_buy_menu'
-    )
+    builder.button(text="Вернуться назад", callback_data="go_buy_menu")
 
     builder.adjust(1)
 
