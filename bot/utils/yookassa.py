b852d99617fd859664a780c1c21e7208e22ef2dc
Khaiam <khaiam@mail.ru>
2024-05-01 20:54:48 +0300

    amount

--- bot/utils/yookassa.py
+++ bot/utils/yookassa.py
@@ -7,6 +7,19 @@ def create_payment(product_title: str, product_price: int, bot_link: str):
     yookassa.Configuration.account_id = getenv("YOOKASSA_API_ID")
     yookassa.Configuration.secret_key = getenv("YOOKASSA_API_KEY")
 
+    print(
+        {
+            "amount": {"value": f"{product_price}.00", "currency": "RUB"},
+            "confirmation": {
+                "type": "redirect",
+                "return_url": bot_link,
+            },
+            "description": product_title,
+            "capture": True,
+            "receipt": None,
+        }
+    )
+
     payment = yookassa.Payment().create(
         {
             "amount": {"value": f"{product_price}.00", "currency": "RUB"},
